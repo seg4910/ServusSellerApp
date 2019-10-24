124c92e222cce3bfe5a473f2a5b082184dba384c
owenadley <oadle049@uottawa.ca>
2019-10-24 11:53:12 -0400

    Added Home & Acc UI

--- components/views/appViews/AccountView.js
+++ components/views/appViews/AccountView.js
@@ -2,8 +2,11 @@ import React, { Component } from "react";
 import {
   Text,
   View,
-  Button
+  Button,
+  TouchableOpacity
 } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+
 class AccountView extends Component {
     constructor(props) {
         super(props);
@@ -13,36 +16,63 @@ class AccountView extends Component {
         }
     };
 
-    goToEditAccountInfo = () => {
+    handleChoosePhoto = () => {}
+
+    editAccountInfo = () => {
       this.props.navigation.navigate("EditAccountInfo");   
-    }
-    goToCreateService = (id, name) => {
-      this.props.navigation.navigate("CreateService", {
-        id: id,
-        name: name,
-      });   
     }
 
   render() {
 
     const { photo } = this.state;
     return (
-        <View style={st.container}>
-            <Text style={st.heading1}>Your Account</Text>
-            <Text style={st.heading2}>{this.props.name}</Text>
-            <Text style={st.heading2}>{this.props.email}</Text>
-            <Button title='Edit Info' onPress={() => this.goToEditAccountInfo()}/>
-            <Button title='Payment Info' onPress={this.props.paymentInfo}/>
-            <Button title='Create service' onPress={() => this.goToCreateService(this.props.id, this.props.name)}/>
+      <View style={{flex:1}}>
+          <View style={{marginBottom:40, padding:20, borderBottomColor:'#dfe6e9', borderBottomWidth:2}}>       
+            <Text style={{fontSize:25, fontWeight:'bold'}}>{this.props.name}</Text>
+            <Text style={{color:'#7f8c8d'}}>{this.props.phone}</Text>
+            <Text style={{color:'#7f8c8d'}}>{this.props.email}</Text>
+          </View>
 
-            {photo && (
-            <Image
-            source={{ uri: photo.uri }}
-            style={{ width: 300, height: 300 }}
-            />
-            )} 
-            <Button title="Choose Photo" onPress={this.props.handleChoosePhoto} />  
-        </View>
+          <TouchableOpacity
+                  style={{borderBottomWidth:1,borderBottomColor:'#dfe6e9',padding:20, flexDirection:'row'}}
+                  onPress={() => this.editAccountInfo()}>
+                  <View style={{flexDirection:'row', flex:1, marginLeft:15}}>
+                    <Icon style={{alignSelf:'center'}} name="edit" size={23} />                  
+                    <Text style={{fontSize:20,paddingLeft:20}}>Edit Account</Text>
+                  </View>
+                  <Icon style={{alignSelf:'center', paddingRight:20}} name="chevron-right" size={18} />                  
+          </TouchableOpacity>     
+
+          <TouchableOpacity
+                  style={{borderBottomWidth:1,borderBottomColor:'#dfe6e9',padding:20, flexDirection:'row'}}
+                  onPress={() => this.editAccountInfo()}>
+                  <View style={{flexDirection:'row', flex:1, marginLeft:17}}>
+                    <Icon style={{alignSelf:'center'}} name="unlock-alt" size={23} />                  
+                    <Text style={{fontSize:20,paddingLeft:20}}>Change Password</Text>
+                  </View>
+                  <Icon style={{alignSelf:'center', paddingRight:20}} name="chevron-right" size={18} />                  
+          </TouchableOpacity>   
+
+          <TouchableOpacity
+                  style={{borderBottomWidth:1,borderBottomColor:'#dfe6e9',padding:20, flexDirection:'row'}}
+                  onPress={this.props.paymentInfo}>
+                  <View style={{flexDirection:'row', flex:1, marginLeft:12}}>
+                    <Icon style={{alignSelf:'center'}} name="credit-card" size={23} />                  
+                    <Text style={{fontSize:20,paddingLeft:20}}>Payment Info</Text>
+                  </View>
+                  <Icon style={{alignSelf:'center', paddingRight:20}} name="chevron-right" size={18} />                  
+          </TouchableOpacity>  
+
+          <TouchableOpacity
+                  style={{borderBottomWidth:1,borderBottomColor:'#dfe6e9',padding:20, flexDirection:'row'}}
+                  onPress={this.handleChoosePhoto}>
+                  <View style={{flexDirection:'row', flex:1, marginLeft:17}}>
+                    <Icon style={{alignSelf:'center'}} name="user" size={23} />                  
+                    <Text style={{fontSize:20,paddingLeft:20}}>Account Photo</Text>
+                  </View>
+                  <Icon style={{alignSelf:'center', paddingRight:20}} name="chevron-right" size={18} />                  
+          </TouchableOpacity>      
+      </View>
     );
   }
 }
